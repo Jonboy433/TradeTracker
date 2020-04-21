eb0e79d2f99987b31ae142f46b118db40e7b6a3a
John Greco <jonboy433@hotmail.com>
2020-04-21 15:30:28 -0400

    Added a quick navbar mockup for testing purposes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Trades from '../Trades/Trades'
+import Navigation from '../layout/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-          <Trades />
+          <Navigation />
       </header>
+      <Route path="/" exact={true} component={Trades} />
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+
+function Navigation() {
+    return (
+        <div>
+            <Navbar bg="light" expand="sm" margin="mb-4">
+                <Navbar.Brand href="/">TradeTracker</Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/home">Home</Nav.Link>
+                    <Nav.Link href="/login">Login</Nav.Link>
+                </Nav>
+            </Navbar>
+        </div>
+    )
+}
+
+export default Navigation
